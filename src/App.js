2bcb29f95c42ab4eafc30009e6523386494edb88
John O'Hara <johnoharaa@gmail.com>
2023-01-12 19:53:18 -0800

    Fixed eslint error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import './App.scss';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App hello-world">
       <h1>Easy Poll App!</h1>
 
-      <p>Hey, you've got a local copy running!</p>
+      <p>Hey, you&apos;ve got a local copy running!</p>
     </div>
   );
 }
