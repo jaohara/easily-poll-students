310f860ec1978e78a8e7304af26429deb2a4a681
John O'Hara <johnoharaa@gmail.com>
2023-01-12 17:49:24 -0800

    Setup React in project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    // TODO: Remove .hello-world 
+    <div className="App hello-world">
+      <h1>Easy Poll App!</h1>
+
+      <p>Hey, you've got a local copy running!</p>
+    </div>
+  );
+}
+
+export default App;
