62102fcfc7a7c0899bec73e2e13bbc81afa4c5ad
vzwork <vladislav.zakharov.work@gmail.com>
2023-02-09 20:13:36 -0800

    no auth

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -40,3 +40,111 @@ export const deleteColorSwatch = /* GraphQL */ `
     }
   }
 `;
+export const createSession = /* GraphQL */ `
+  mutation CreateSession(
+    $input: CreateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    createSession(input: $input, condition: $condition) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateSession = /* GraphQL */ `
+  mutation UpdateSession(
+    $input: UpdateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    updateSession(input: $input, condition: $condition) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteSession = /* GraphQL */ `
+  mutation DeleteSession(
+    $input: DeleteSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    deleteSession(input: $input, condition: $condition) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createQuestion = /* GraphQL */ `
+  mutation CreateQuestion(
+    $input: CreateQuestionInput!
+    $condition: ModelQuestionConditionInput
+  ) {
+    createQuestion(input: $input, condition: $condition) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
+export const updateQuestion = /* GraphQL */ `
+  mutation UpdateQuestion(
+    $input: UpdateQuestionInput!
+    $condition: ModelQuestionConditionInput
+  ) {
+    updateQuestion(input: $input, condition: $condition) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
+export const deleteQuestion = /* GraphQL */ `
+  mutation DeleteQuestion(
+    $input: DeleteQuestionInput!
+    $condition: ModelQuestionConditionInput
+  ) {
+    deleteQuestion(input: $input, condition: $condition) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -37,3 +37,93 @@ export const onDeleteColorSwatch = /* GraphQL */ `
     }
   }
 `;
+export const onCreateSession = /* GraphQL */ `
+  subscription OnCreateSession($filter: ModelSubscriptionSessionFilterInput) {
+    onCreateSession(filter: $filter) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateSession = /* GraphQL */ `
+  subscription OnUpdateSession($filter: ModelSubscriptionSessionFilterInput) {
+    onUpdateSession(filter: $filter) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteSession = /* GraphQL */ `
+  subscription OnDeleteSession($filter: ModelSubscriptionSessionFilterInput) {
+    onDeleteSession(filter: $filter) {
+      id
+      questions {
+        items {
+          id
+          prompt
+          createdAt
+          updatedAt
+          sessionQuestionsId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateQuestion = /* GraphQL */ `
+  subscription OnCreateQuestion($filter: ModelSubscriptionQuestionFilterInput) {
+    onCreateQuestion(filter: $filter) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
+export const onUpdateQuestion = /* GraphQL */ `
+  subscription OnUpdateQuestion($filter: ModelSubscriptionQuestionFilterInput) {
+    onUpdateQuestion(filter: $filter) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
+export const onDeleteQuestion = /* GraphQL */ `
+  subscription OnDeleteQuestion($filter: ModelSubscriptionQuestionFilterInput) {
+    onDeleteQuestion(filter: $filter) {
+      id
+      prompt
+      createdAt
+      updatedAt
+      sessionQuestionsId
+    }
+  }
+`;
