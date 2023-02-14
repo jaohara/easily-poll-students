8069768d1946056fd9cb15ec41941a0bab8e99ba
John O'Hara <johnoharaa@gmail.com>
2023-02-14 08:54:56 -0800

    Added override to eslint for typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,20 @@ module.exports = {
         "plugin:react/recommended"
     ],
     "overrides": [
+        {
+            "files": ['*.ts', '*.tsx'],
+            "extends": [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            "parserOptions": {
+                "project": ['./tsconfig.json'],
+            },
+        },
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module",
-        "project": "./tsconfig.json",
-        // "project": true,
-        // "tsconfigRootDir"
     },
     "plugins": [
         "react"
