303968746625db630e6981b3c9cd285bc6cae903
John O'Hara <johnoharaa@gmail.com>
2023-02-13 18:26:32 -0800

    Fixed typo in comment

--- src/components/UI/EpTextInput/EpTextInput.jsx
+++ src/components/UI/EpTextInput/EpTextInput.jsx
@@ -5,7 +5,7 @@ import './EpTextInput.scss';
 import TextField from '@mui/material/TextField';
 
 /*
-  This is a stub for our wrapped version of the Material UI button.
+  This is a stub for our wrapped version of the Material UI TextField.
 */
 
 const EpTextInput = (props) => {
