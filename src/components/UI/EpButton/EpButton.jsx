9d7973664bf7d2cf21bddade5598887c783258d6
John O'Hara <johnoharaa@gmail.com>
2023-02-13 18:15:23 -0800

    Created stubs for wrapping common UI elements

---
+++ src/components/UI/EpButton/EpButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './EpButton.scss';
+
+import Button from '@mui/material/Button';
+
+/*
+  This is a stub for our wrapped version of the Material UI button.
+*/
+
+const EpButton = (props) => {
+  return (
+    <Button {...props} />
+  );
+}
+ 
+export default EpButton;
---
+++ src/components/UI/EpTextInput/EpTextInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './EpTextInput.scss';
+
+import TextField from '@mui/material/TextField';
+
+/*
+  This is a stub for our wrapped version of the Material UI button.
+*/
+
+const EpTextInput = (props) => {
+  return (
+    <TextField {...props} />
+  );
+}
+ 
+export default EpTextInput;
