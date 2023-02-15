a47c326248930c261f0028dbe92b89af386f4185
vzwork <100447833+vzwork@users.noreply.github.com>
2023-02-15 07:45:58 -0800

    Revert "auth api restrict test"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import './index.css';
-import App from "./App";
-import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports";
-Amplify.configure(awsconfig);
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
--- src/routes.js
+++ src/routes.js
@@ -1,46 +1,28 @@
-import React from "react";
+import React from 'react';
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./components/pages/Home/Home";
-import MaterialUISwatch from "./components/MaterialUISwatch/MaterialUISwatch";
-import ColorSwatchTest from "./components/ColorSwatchTest/ColorSwatchTest";
-import ColorSwatchHookTest from "./components/Demo/ColorSwatchTest/ColorSwatchHookTest";
-import EpChartDemo from "./components/Demo/EpChartDemo/EpChartDemo";
-import Login from "./components/pages/Auth/Login/Login";
-import Register from "./components/pages/Auth/Register/Register";
-import TestApi from "./components/pages/TestApi/TestApi";
+import MaterialUISwatch from "./components/Demo/MaterialUISwatch/MaterialUISwatch";
+import ColorSwatchTest from "./components/Demo/ColorSwatchTest/ColorSwatchTest";
+import ColorSwatchHookTest from './components/Demo/ColorSwatchTest/ColorSwatchHookTest';
+import EpChartDemo from './components/Demo/EpChartDemo/EpChartDemo';
 
 // name is display name on button/link
 export const routes = [
   {
     path: "/",
     name: "Home",
-    element: <Home />,
+    element: <Home />
   },
   {
     path: "/material-demo",
     name: "Material UI Swatch",
-    element: <MaterialUISwatch />,
+    element: <MaterialUISwatch />
   },
   {
     path: "/color-swatch-demo",
     name: "Color Swatch Demo",
-    element: <ColorSwatchTest />,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    name: "Register",
-    element: <Register />,
-  },
-  {
-    path: "/testapi",
-    name: "Test Api",
-    element: <TestApi />,
+    element: <ColorSwatchTest />
   },
   {
     path: "/color-swatch-demo-hooks",
@@ -51,7 +33,7 @@ export const routes = [
     path: "/epchart-demo",
     name: "chart demo",
     element: <EpChartDemo />
-  },
+  }
 ];
 
 // create router, filtering out names
