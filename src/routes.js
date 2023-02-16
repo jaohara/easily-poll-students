3c1ff8a82c7a66177dec3407641ad81540e21811
John O'Hara <johnoharaa@gmail.com>
2023-02-16 10:55:01 -0800

    Removed Demo components from main

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./components/pages/Home/Home";
 import MaterialUISwatch from "./components/Demo/MaterialUISwatch/MaterialUISwatch";
-import ColorSwatchTest from "./components/Demo/ColorSwatchTest/ColorSwatchTest";
-import ColorSwatchHookTest from './components/Demo/ColorSwatchTest/ColorSwatchHookTest';
 import EpChartDemo from './components/Demo/EpChartDemo/EpChartDemo';
 
 // name is display name on button/link
@@ -18,16 +16,6 @@ export const routes = [
     path: "/material-demo",
     name: "Material UI Swatch",
     element: <MaterialUISwatch />
-  },
-  {
-    path: "/color-swatch-demo",
-    name: "Color Swatch Demo",
-    element: <ColorSwatchTest />
-  },
-  {
-    path: "/color-swatch-demo-hooks",
-    name: "Color Swatch Demo w/Hooks",
-    element: <ColorSwatchHookTest />
   },
   {
     path: "/epchart-demo",
