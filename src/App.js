085d01a915152d0f62e04595f926c6934ff9ab1b
John O'Hara <johnoharaa@gmail.com>
2023-02-17 10:53:50 -0800

    Added Vlad's auth context and components

--- src/App.js
+++ src/App.js
@@ -15,26 +15,32 @@ import {
   routes
 } from './routes';
 
+
+import { AuthContextProvider } from "./contexts/AuthContext/AuthContext";
+
 import EpNavBar from './components/UI/EpNavBar/EpNavBar';
 
 function App () {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <h1>Easy Poll App!</h1>
-          <EpNavBar />
+        <AuthContextProvider>
 
-          <p>This is <strong>another test</strong> of the new CI/CD approach.</p>
+          <div className="App">
+            <h1>Easy Poll App!</h1>
+            <EpNavBar />
 
-          <Routes>
-            {
-              routes.map((route, index) => (
-                <Route path={route.path} element={route.element} key={`route-${index}`}/>
-              ))
-            }
-          </Routes>
-        </div>
+            <p>This is <strong>another test</strong> of the new CI/CD approach.</p>
+
+            <Routes>
+              {
+                routes.map((route, index) => (
+                  <Route path={route.path} element={route.element} key={`route-${index}`}/>
+                  ))
+                }
+            </Routes>
+          </div>
+        </AuthContextProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
