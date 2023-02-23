cfdf492c1ec1a907fa44ea6f4f656db4f77e63bd
Taylor Papke <taylorpapke@gmail.com>
2023-02-23 01:32:25 -0800

    updating chart demo to accept data from answerTally

--- src/components/Demo/EpChartDemo/EpChartDemo.jsx
+++ src/components/Demo/EpChartDemo/EpChartDemo.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
-import { useState } from "react";
-import { Data } from "./SampleData";
+//import { useState } from "react";
+//import { Data } from "./SampleData";
 import EpChart from '../../UI/EpChart/EpChart';
 
+import useQuestionData from '../../../hooks/useQuestionData';
+
 const EpChartDemo = () => {
-  const [chartData] = useState({
-    labels: Data.map((data) => data.year), 
-    datasets: [
-      {
-        label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-        
-      }
-    ]
+  const { answerTally } = useQuestionData({
+    questionId: "d3a126ab-292a-4d0d-bee9-d4c73d19016d"
   });
 
   return (
+    <>
+    {
+      answerTally && (
     <div className="App">
-      <EpChart chartData={chartData} />
+      <EpChart chartData={{
+        //labels: Data.map((data) => data.year), 
+        labels: answerTally.labels,
+        datasets: [
+          {
+            //label: "Users Gained ",
+            data: answerTally.data,
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0"
+            ],
+            borderColor: "black",
+            borderWidth: 2,
+          }
+        ]
+      }} />
     </div>
+  )
+    }
+    </>
   );
 }
    
