4889e9e55442486dad1c6ab5c8fd73dbf6636d83
Taylor Papke <taylorpapke@gmail.com>
2023-02-24 15:01:51 -0800

    display pie and bar in EpChartDemo

--- src/components/Demo/EpChartDemo/EpChartDemo.jsx
+++ src/components/Demo/EpChartDemo/EpChartDemo.jsx
@@ -5,7 +5,7 @@ import EpChart from '../../UI/EpChart/EpChart';
 
 import useQuestionData from '../../../hooks/useQuestionData';
 
-const EpChartDemo = ({chartType}) => {
+const EpChartDemo = () => {
   const { answerTally } = useQuestionData({
     questionId: "d3a126ab-292a-4d0d-bee9-d4c73d19016d"
   });
@@ -15,7 +15,26 @@ const EpChartDemo = ({chartType}) => {
     {
       answerTally && (
     <div className="App">
-      <EpChart chartType={chartType} chartData={{
+      <EpChart chartType={'pie'} chartData={{
+        //labels: Data.map((data) => data.year), 
+        labels: answerTally.labels,
+        datasets: [
+          {
+            //label: "Users Gained ",
+            data: answerTally.data,
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0"
+            ],
+            borderColor: "black",
+            borderWidth: 2,
+          }
+        ]
+      }} />
+      <EpChart chartType={'bar'} chartData={{
         //labels: Data.map((data) => data.year), 
         labels: answerTally.labels,
         datasets: [
