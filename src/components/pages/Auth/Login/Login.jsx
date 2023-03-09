9c12eaa65d48cefea0430ed6ee964f2e9ff6c474
John O'Hara <johnoharaa@gmail.com>
2023-03-08 19:04:33 -0800

    Replaced login with loading spinner when waiting for response

--- src/components/pages/Auth/Login/Login.jsx
+++ src/components/pages/Auth/Login/Login.jsx
@@ -1,4 +1,6 @@
-import * as React from 'react'
+import React, {
+  useState,
+} from 'react'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
@@ -14,11 +16,16 @@ import { AuthContext } from '../../../../contexts/AuthContext/AuthContext'
 import "../Auth.scss";
 
 import EpContainer from "../../../UI/EpContainer/EpContainer";
+import EpLoading from '../../../UI/EpLoading/EpLoading'
 
 export default function Login() {
-  const Auth = React.useContext(AuthContext)
+  const [ loginSubmitted, setLoginSubmitted ] = useState(false);
+
+  const Auth = React.useContext(AuthContext);
 
   const handleSubmit = async (event) => {
+    //TODO: reset this if login goes bad
+    setLoginSubmitted(true);
     event.preventDefault()
     const data = new FormData(event.currentTarget)
     await Auth.login(data.get('email'), data.get('password'))
@@ -26,69 +33,75 @@ export default function Login() {
 
   return (
     <div className="login-page">
-      <EpContainer narrow centered>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-          >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Login
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="/recover" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </EpContainer>
+      {
+        loginSubmitted ? (
+          <EpLoading />
+        ) : (
+          <EpContainer narrow centered>
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+              >
+              <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Login
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/recover" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/register" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </EpContainer>
+        )
+      }
     </div>
   )
 }
