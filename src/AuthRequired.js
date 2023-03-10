564d841ad6f77a1d04382e78a6b99bab2b656e8d
John O'Hara <johnoharaa@gmail.com>
2023-03-09 22:24:39 -0800

    Fixed auth redirect and login stuff

--- src/AuthRequired.js
+++ src/AuthRequired.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {
+  useContext, useEffect,
+} from 'react';
 import { useLocation, Navigate } from "react-router-dom";
-import { useAuthenticator } from "@aws-amplify/ui-react";
+// import { useAuthenticator } from "@aws-amplify/ui-react";
+
+import { AuthContext } from './contexts/AuthContext/AuthContext';
 
 export function AuthRequired({ children }) {
   const location = useLocation();
-  const { route } = useAuthenticator((context) => [context.route]);
-  if (route !== "authenticated") {
+  // const { route } = useAuthenticator((context) => [context.route]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    console.log("location: ", location);
+  }, []);
+
+  // if (route !== "authenticated") {
+  if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
   return children;
